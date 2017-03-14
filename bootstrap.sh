5793174788b5cb3b4953f5e9cabecd6ea449f778
Seth Deckard <seth@deckard.me>
2017-03-14 18:13:50 -0500

    add setup script

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.gitignore_global ~/.gitignore_global
+ln -sf $PWD/.irbrc ~/.irbrc
+ln -sf $PWD/.ssh_config ~/.ssh/config
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.vimrc ~/.vimrc
+ln -sf $PWD/.zshenv ~/.zshenv
+ln -sf $PWD/.zshrc ~/.zshrc
+ln -sf $PWD/default-gems $(echo $HOME/.rbenv)/default-gems
