3a25b15f485d2b1e6edd0a140aac05f60f310921
Seth Deckard <seth@deckard.me>
2023-09-15 07:35:10 -0400

    Add alias for git branch --unset-upstream

--- zsh_custom/aliases/main.zsh
+++ zsh_custom/aliases/main.zsh
@@ -7,6 +7,7 @@ alias cxr='convox rack'
 # relying on omzsh plugin for most of of these and overriding or extending here
 # note: gitconfig includes some under git command (or just `g`, ex: `g br`).
 alias gbm='git branch -mv'
+alias gbuu='git branch --unset-upstream'
 alias gcdf='git clean -d -f'
 alias gsur='git submodule update --recursive'
 alias gswd='git switch --detach'
