19fc470c1faaf9c9ba4bba1d62cdad4fb3e2ac94
Seth Deckard <seth@deckard.me>
2025-09-26 15:37:18 -0400

    Add fallback to first project

--- zsh_custom/plugins/xcworkspace/xcworkspace.plugin.zsh
+++ zsh_custom/plugins/xcworkspace/xcworkspace.plugin.zsh
@@ -2,8 +2,13 @@
 function oxc() {
   local workspace=$(find . -maxdepth 1 -name "*.xcworkspace" | head -n 1)
 
+  # fallback to opening first project
   if [[ -z "$workspace" ]]; then
-    echo "No .xcworkspace file found in the current directory."
+    workspace=$(find . -maxdepth 1 -name "*.xcodeproj" | head -n 1)
+  fi
+
+  if [[ -z "$workspace" ]]; then
+    echo "No .xcworkspace or .xcodeproj file found in the current directory."
     return 1
   fi
 
